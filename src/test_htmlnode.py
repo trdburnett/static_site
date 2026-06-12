e030040719a3f0e665b57174bbdfade4f33f16c4
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-12 23:56:26 +0100

    edited test_htmlnode.py

---
+++ src/test_htmlnode.py
@@ -0,0 +1,12 @@
+import unittest
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def props_to_html_test_empty(self):
+        testnode = HTMLNode()
+        assert(testnode.props_to_html == "")
+
+    def props_to_html_test(self):
+        testnode = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
+        assert(testnode.__repr__ == "href\"https://www.google.com\" target=\"_blank")
+        
