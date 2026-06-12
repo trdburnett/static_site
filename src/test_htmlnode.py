a7596cd0d903cb5fb7c68e7d3a6ee917f9c80a1f
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-13 00:58:23 +0100

    fixed typo in test_htmlnode.py

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -8,7 +8,7 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_props_to_html(self):
         testnode = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
-        assert(testnode.props_to_html() == "href\"https://www.google.com\" target=\"_blank")
+        assert(testnode.props_to_html() == "href=\"https://www.google.com\" target=\"_blank")
 
 if __name__ == "__main__":
     unittest.main()
