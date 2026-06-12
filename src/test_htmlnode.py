4dbc240aac11f03f6a75cd74bc579f04e5763e9f
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-12 23:57:52 +0100

    edited test_htmlnode.py

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -9,4 +9,6 @@ class TestHTMLNode(unittest.TestCase):
     def props_to_html_test(self):
         testnode = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
         assert(testnode.__repr__ == "href\"https://www.google.com\" target=\"_blank")
-        
+
+if __name__ == "__main__":
+    unittest.main()
