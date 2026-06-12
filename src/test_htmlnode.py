8770eac8cfaf24fce6fb15f565ce36501b4ef5c9
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-13 00:09:41 +0100

    edited test_htmlnode.py

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,13 +2,13 @@ import unittest
 from htmlnode import HTMLNode
 
 class TestHTMLNode(unittest.TestCase):
-    def props_to_html_test_empty(self):
+    def test_props_to_html_empty(self):
         testnode = HTMLNode()
-        assert(testnode.props_to_html == "")
+        assert(testnode.props_to_html() == "")
 
-    def props_to_html_test(self):
+    def test_props_to_html(self):
         testnode = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
-        assert(testnode.__repr__ == "href\"https://www.google.com\" target=\"_blank")
+        assert(testnode.__repr__() == "href\"https://www.google.com\" target=\"_blank")
 
 if __name__ == "__main__":
     unittest.main()
