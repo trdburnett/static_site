5a70413295aa7f5443c37eb469c3e5da02ce45b3
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-13 01:25:57 +0100

    edited test_htmlnode.py

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -11,8 +11,8 @@ class TestHTMLNode(unittest.TestCase):
         assert(testnode.props_to_html() == " href=\"https://www.google.com\" target=\"_blank\"")
 
     def test_repr(self):
-        testnode = HTMLNode("p", "hello", None, {"class": "primary"})
-        expected = "HTML_Node(p, hello, children: None, {\"class\": \"primary\"})"
+        testnode = HTMLNode("p", "hello", None, {'class': 'primary'})
+        expected = "HTML_Node(p, hello, children: None, {\'class\': \'primary\'})"
         self.assertEqual(repr(testnode), expected)
 
 if __name__ == "__main__":
