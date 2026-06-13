f959f5a671b481a410d3b54abea535c08ccaf57c
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-13 23:24:59 +0100

    added tests for leafnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_empty(self):
@@ -14,6 +14,20 @@ class TestHTMLNode(unittest.TestCase):
         testnode = HTMLNode("p", "hello", None, {'class': 'primary'})
         expected = "HTML_Node(p, hello, children: None, {\'class\': \'primary\'})"
         self.assertEqual(repr(testnode), expected)
+
+    def test_leaf_to_html_p(self):
+        testnode = LeafNode("p", "Hello, world!")
+        expected = "<p>Hello, world!"
+        self.assertEqual(testnode.to_html(), expected)
+
+    def test_leaf_to_html_no_tag(self):
+        testnode = LeafNode(None, "Hello, world!")
+        expected = "Hello, world!"
+        self.assertEqual(testnode.to_html(), expected)
+
+    def test_leaf_to_html_no_value(self):
+        testnode = LeafNode(None, None)
+        self.assertRaises(ValueError, testnode.to_html())
 
 if __name__ == "__main__":
     unittest.main()
