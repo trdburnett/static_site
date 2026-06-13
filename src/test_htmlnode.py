44cca3fe6bf996c626f6ab9af9f3550846ab51bd
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-13 01:15:29 +0100

    added repr test to test_htmlnode.py

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -10,5 +10,12 @@ class TestHTMLNode(unittest.TestCase):
         testnode = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
         assert(testnode.props_to_html() == " href=\"https://www.google.com\" target=\"_blank\"")
 
+    def test_repr(self):
+        testnode = HTMLNode("p", "hello", None, {"class": "primary"})
+        self.assertEqual(
+            repr(testnode),
+            "HTML_Node(p, hello, children: None, {"class": "primary"})"
+        )
+
 if __name__ == "__main__":
     unittest.main()
