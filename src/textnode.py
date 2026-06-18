cd5c6a94b3885ffd06461fab9ec1c464eaca6b69
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-19 00:48:24 +0100

    fixed comparisons

--- src/textnode.py
+++ src/textnode.py
@@ -27,15 +27,15 @@ def text_node_to_html_node(text_node: TextNode) -> LeafNode:
         raise(Exception("Invalid Text Type"))
     if text_node.text_type == TextType.TEXT:
         return LeafNode(tag=None, value=text_node.text)
-    if text_node.text_type == "bold":
+    if text_node.text_type == TextType.BOLD:
         return LeafNode(tag="b", value=text_node.text)
-    if text_node.text_type == "italic":
+    if text_node.text_type == TextType.ITALIC:
         return LeafNode(tag="i", value=text_node.text)
-    if text_node.text_type == "code":
+    if text_node.text_type == TextType.CODE:
         return LeafNode(tag="code", value=text_node.text)
-    if text_node.text_type == "link":
+    if text_node.text_type == TextType.LINK:
         return LeafNode(tag="a", value=text_node.text, props={"href"})
-    if text_node.text_type == "image":
+    if text_node.text_type == TextType.IMAGE:
         return LeafNode(tag="img", value="", props={"src","alt"})
     
     
