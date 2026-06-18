b87bdcc0a7e9d0f06374c6f82d8120df19511559
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-18 21:46:45 +0100

    debugging

--- src/debug.py
+++ src/debug.py
@@ -1,4 +1,5 @@
 from htmlnode import HTMLNode, LeafNode, ParentNode
 child_node = LeafNode("span", "child")
 parent_node = ParentNode("div", [child_node])
+print(parent_node.children)
 print(parent_node.to_html())
