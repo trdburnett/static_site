10613bf465b029efe4263c8b7a7f6483da88dd48
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-19 00:29:20 +0100

    debugging

--- src/debug.py
+++ src/debug.py
@@ -3,4 +3,4 @@ from textnode import TextNode, text_node_to_html_node, TextType
 node = TextNode("This is a text node", TextType.TEXT)
 html_node = text_node_to_html_node(node)
 print(node.text_type)
-print(html_node)
+#print(html_node)
