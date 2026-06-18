4ae8267eeea28b1d318b4d8af1922514a77958d0
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-18 21:56:09 +0100

    debugging

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -38,7 +38,7 @@ class LeafNode(HTMLNode):
     
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
-        super().__init__(tag, children, props)
+        super().__init__(tag=tag, children=children, props=props)
 
     def to_html(self):
         if self.tag == None:
