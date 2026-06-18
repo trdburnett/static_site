20ce4f9cb202d3b68226532528e21747a557b363
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-19 00:24:17 +0100

    added test_text

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,6 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
+from htmlnode import HTMLNode, LeafNode
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -31,6 +32,13 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.LINK, "https://www.boot.dev")
         node2 = TextNode("This is a text node", TextType.LINK, "https://www.shoe.com")
         self.assertNotEqual(node,node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
 
 if __name__ == "__main__":
     unittest.main()
