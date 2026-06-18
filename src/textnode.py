481af2d73aedfe71b4146a7dd7fbd2b5c724c6e7
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-19 00:39:54 +0100

    debugging

--- src/textnode.py
+++ src/textnode.py
@@ -23,7 +23,7 @@ class TextNode():
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
 def text_node_to_html_node(text_node: TextNode) -> LeafNode:
-    if text_node.text_type not in TextType.__members__:
+    if text_node.text_type.TYPE not in TextType.__members__:
         raise(Exception("Invalid Text Type"))
     if text_node.text_type == "text":
         return LeafNode(tag=None, value=text_node.text)
