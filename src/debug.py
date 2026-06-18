2d627a6fd497529eef49ebef21e99e7336b0f3bc
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-19 00:44:02 +0100

    debugging

--- src/debug.py
+++ src/debug.py
@@ -1,6 +1,6 @@
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from textnode import TextNode, text_node_to_html_node, TextType
 node = TextNode("This is a text node", TextType.TEXT)
-#html_node = text_node_to_html_node(node)
+html_node = text_node_to_html_node(node)
 print(node.text_type)
-#print(html_node)
+print(html_node)
