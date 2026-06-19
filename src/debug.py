4dfc456f75759702af49c1922d763bac60f933a7
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-19 20:41:39 +0100

    debugging

--- src/debug.py
+++ src/debug.py
@@ -1,2 +1,5 @@
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from textnode import TextNode, text_node_to_html_node, TextType
+node = TextNode("This is a faulty text node", TextType.INVALID)
+print(node.text_type)
+print(str(node.text_type))
