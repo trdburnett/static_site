7d21727817140a2e348a1356b540919d058fbbd4
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-20 01:29:43 +0100

    debugging

--- src/debug.py
+++ src/debug.py
@@ -1,3 +1,7 @@
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from textnode import TextNode, text_node_to_html_node, TextType
 print(TextType.__members__)
+text_types = []
+for k,v in TextType.__members__:
+    text_types.append(k)
+print(text_types)
