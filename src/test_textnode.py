cf0d43354ad23b71e484ec2bdf44d9efefecd1e8
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-20 23:58:47 +0100

    adding test

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -69,6 +69,12 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.value, "This is a link node")
         self.assertEqual(html_node.props, {'href': 'www.google.co.uk'})
 
+    def test_link_none(self):
+        node = TextNode("This is a link node", TextType.LINK)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link node")
+        self.assertEqual(html_node.props, {'href': 'None'})
+
 
 if __name__ == "__main__":
     unittest.main()
