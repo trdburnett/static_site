a3d11fe1d677b1b2dd6b78708c0a51c5365eab7e
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-20 01:46:08 +0100

    fixed typo

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -44,7 +44,7 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
 
-    def text_bold(self):
+    def test_bold(self):
         node = TextNode("This is a bold node", TextType.BOLD)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
