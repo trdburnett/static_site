e261aa609c4a989a23ec61961dc64e293c7907da
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-20 02:18:09 +0100

    debugging

--- src/debug.py
+++ src/debug.py
@@ -1,7 +1,6 @@
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from textnode import TextNode, text_node_to_html_node, TextType
-print(TextType.__members__)
-text_types = []
-for member in TextType.__members__:
-    text_types.append(member)
-print(text_types)
+node = TextNode("This is a link node", TextType.LINK, url="www.google.co.uk")
+print(node)
+html_node = LeafNode(tag="a", value="This is a link node", props={"href": "www.google.co.uk"})
+print(html_node)
