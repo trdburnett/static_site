edd2a5380f3f6d00fce49e2ef6a43c5cddf1c72c
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-20 01:16:45 +0100

    debugging

--- src/debug.py
+++ src/debug.py
@@ -1,5 +1,5 @@
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from textnode import TextNode, text_node_to_html_node, TextType
-node = TextNode("This is a faulty text node", TextType.INVALID)
-print(node.text_type)
-print(str(node.text_type))
+text_types = []
+for text_type in TextType:
+    text_types.append(text_type)
