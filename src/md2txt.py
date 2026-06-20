8c6dbce9f2e393111ac480b3d70807b9a1deb5a4
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-21 00:35:16 +0100

    working through split delimiter not complete

---
+++ src/md2txt.py
@@ -0,0 +1,9 @@
+from textnode import TextNode, TextType
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            text = node.text.split(delimiter)
+            print(text)
