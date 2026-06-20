744f6d5c6fcc8f11a8625a5e89492d395f2885c7
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-20 02:29:18 +0100

    fixing error with leaf node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -23,7 +23,7 @@ class HTMLNode():
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        super().__init__(tag, value, props)
+        super().__init__(tag, value, None, props)
     
     def to_html(self):
         if self.value == None:
