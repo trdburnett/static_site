40c4fbd3d22afb027be284c32ef1cd32cd37c2e4
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-21 19:30:05 +0100

    editing

--- src/md2txt.py
+++ src/md2txt.py
@@ -5,6 +5,14 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
         if node.text_type != TextType.TEXT:
             new_nodes.append(node)
         else:
+            delimiter_count = 0
+            for char in node.text:
+                if char == delimiter:
+                    delimiter_count += 1
+            print(delimiter_count)
+        if delimiter_count % 2 != 0:
+            raise Exception("Invalid Markdown Syntax")
+        else:
             text = node.text.split(delimiter)
             print(text)
 
