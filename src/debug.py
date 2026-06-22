610a53c413b548ba0670f0a024e93672c9b68e01
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-22 22:39:53 +0100

    debugging

--- src/debug.py
+++ src/debug.py
@@ -3,15 +3,20 @@ from textnode import TextNode, text_node_to_html_node, TextType
 text = "This is text with a *bold** word"
 delimiter = "**"
 delimiter_count = 0
+print(delimiter[0])
+print(delimiter[1])
 if len(delimiter) == 2:
     check_next_char = False
     for char in text:
         if not check_next_char: 
             if char == delimiter[0]:
                 check_next_char = True
+                print("Found first half of delimiter")
             if check_next_char:
                 if char == delimiter[1]:
+                    print("Found second hald of delimiter")
                     delimiter_count +=1
+                print("setting check next char to false")    
                 check_next_char = False
 print(delimiter_count)
 if len(delimiter) == 1:        
