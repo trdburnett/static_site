be6ef26406d6ab31e9181e277199b0f1e8f2d196
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-22 22:44:26 +0100

    debugging

--- src/debug.py
+++ src/debug.py
@@ -8,16 +8,16 @@ print(delimiter[1])
 if len(delimiter) == 2:
     check_next_char = False
     for char in text:
-        if not check_next_char: 
-            if char == delimiter[0]:
-                check_next_char = True
-                print("Found first half of delimiter")
         if check_next_char:
             if char == delimiter[1]:
                 print("Found second hald of delimiter")
                 delimiter_count +=1
             print("setting check next char to false")    
             check_next_char = False
+        if not check_next_char: 
+            if char == delimiter[0]:
+                check_next_char = True
+                print("Found first half of delimiter")
 print(delimiter_count)
 if len(delimiter) == 1:        
     for char in text:
