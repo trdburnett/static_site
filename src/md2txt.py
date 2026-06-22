1be8f6937c748944d7927078461e2326bb7bd390
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-22 20:21:16 +0100

    updated md2txt.py to handle empty list input with exception

--- src/md2txt.py
+++ src/md2txt.py
@@ -1,6 +1,8 @@
 from textnode import TextNode, TextType
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
     new_nodes = []
+    if old_nodes == []:
+        raise Exception("Empty Input List")
     for node in old_nodes:
         if node.text_type != TextType.TEXT:
             new_nodes.append(node)
