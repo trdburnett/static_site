50b6e2259696e886332e45a386a505342dfb4730
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-22 20:47:30 +0100

    added exception for handling when a delimiter is not found

--- src/md2txt.py
+++ src/md2txt.py
@@ -11,8 +11,10 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
             for char in node.text:
                 if char == delimiter:
                     delimiter_count += 1
-            if delimiter_count % 2 != 0 or delimiter_count == 0:
+            if delimiter_count % 2 != 0:
                 raise Exception("Invalid Markdown Syntax")
+            elif delimiter_count == 0:
+                raise Exception("Delimiter not found")
             else:
                 text = node.text.split(delimiter,maxsplit=(delimiter_count))
                 for i in range(0,len(text)):
