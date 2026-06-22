0507135649ef09d9805dacdfaa4b9953159e7d89
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-22 23:53:24 +0100

    debugging

--- src/debug.py
+++ src/debug.py
@@ -1,28 +1,45 @@
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from textnode import TextNode, text_node_to_html_node, TextType
-text = "This is text with a *bold** word"
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    new_nodes = []
+    if old_nodes == []:
+        raise Exception("Empty Input List")
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            delimiter_count = 0
+            if len(delimiter) > 2:
+                raise Exception("Invalid Delimiter")
+            if len(delimiter) == 2:
+                check_next_char = False
+                for char in node.text:
+                    if check_next_char:
+                        if char == delimiter[1]:
+                            delimiter_count +=1
+                        check_next_char = False
+                    if not check_next_char: 
+                        if char == delimiter[0]:
+                            check_next_char = True
+            if len(delimiter) == 1:        
+                for char in node.text:
+                    if char == delimiter:
+                        delimiter_count += 1
+            if delimiter_count % 2 != 0:
+                raise Exception("Invalid Markdown Syntax")
+            elif delimiter_count == 0:
+                raise Exception("Delimiter not found")
+            else:
+                text = node.text.split(delimiter,maxsplit=(delimiter_count))
+                for i in range(0,len(text)):
+                    if i % 2 == 0:
+                        new_nodes.append(TextNode(text[i], TextType.TEXT))
+                    else:
+                        new_nodes.append(TextNode(text[i], text_type))
+    return new_nodes
+
+old_nodes = [TextNode("This is text with a *bold** word", TextType.BOLD)]
 delimiter = "**"
-delimiter_count = 0
-print(delimiter[0])
-print(delimiter[1])
-if len(delimiter) == 2:
-    check_next_char = False
-    for char in text:
-        if check_next_char:
-            if char == delimiter[1]:
-                print("Found second hald of delimiter")
-                delimiter_count +=1
-            print("setting check next char to false")    
-            check_next_char = False
-        if not check_next_char: 
-            if char == delimiter[0]:
-                check_next_char = True
-                print("Found first half of delimiter")
-print(delimiter_count)
-if len(delimiter) == 1:        
-    for char in text:
-        if char == delimiter:
-            delimiter_count += 1
-print(delimiter_count)
-if delimiter_count % 2 != 0:
-    print("Hi!")
+text_type = TextType.BOLD
+new_nodes = split_nodes_delimiter(old_nodes,delimiter,text_type)
+print(new_nodes)
