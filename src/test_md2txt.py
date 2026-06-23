1d1d2ef22fb96c7173fc68dde34138e51a49d08f
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-23 12:19:39 +0100

    added tests

--- src/test_md2txt.py
+++ src/test_md2txt.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from md2txt import split_nodes_delimiter
+from md2txt import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 class Testmd2txt(unittest.TestCase):
     def test_empty_list(self):
@@ -97,6 +97,16 @@ class Testmd2txt(unittest.TestCase):
         self.assertEqual(new_nodes, [TextNode("This is text with an ", TextType.TEXT),
                                      TextNode("italic", TextType.ITALIC),
                                      TextNode(" word", TextType.TEXT)])
+        
+    def test_extract_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        matches = extract_markdown_images(text)
+        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], matches)
+
+    def test_extract_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        matches = extract_markdown_links(text)
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
 
 if __name__ == "__main__":
     unittest.main()
