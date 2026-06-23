f753ec04e0bfcaf1ef1cf66138f4c39f96a4e929
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-23 19:50:10 +0100

    working on split_nodes_image

--- src/md2txt.py
+++ src/md2txt.py
@@ -43,4 +43,16 @@ def extract_markdown_images(text: str) -> list[tuple]:
 
 def extract_markdown_links(text: str) -> list[tuple]:
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
-    
+
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    if old_nodes == []:
+        raise Exception("Empty Input List")
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            print(extract_markdown_images(node.text))
+
+old_nodes = [TextNode("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)", TextType.TEXT)]
+split_nodes_image(old_nodes)
