b7506ea16af134967a61257cbb32c82efa7489b1
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-26 22:51:47 +0100

    editing

--- src/md2txt.py
+++ src/md2txt.py
@@ -144,4 +144,4 @@ def text_to_textnodes(text: str) -> list[TextNode]:
     #print(result)
 
 
-text_to_textnodes("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
+#text_to_textnodes("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
