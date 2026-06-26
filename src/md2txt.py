b7067469034fc45b3d4cca18063279479e8d348c
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-26 22:37:04 +0100

    editing

--- src/md2txt.py
+++ src/md2txt.py
@@ -63,7 +63,7 @@ def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
                 new_nodes.append(TextNode(split_node[1], TextType.TEXT))
             if len(extracted) == 1:
                 return new_nodes
-            else:
+            elif len(extracted) > 1:
                 return split_nodes_image(new_nodes)
             #else:
             #    split_text = node.text.split("!")
