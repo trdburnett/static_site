4ee7aa9901444831ce19d37fc0ab7a666df1128b
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-27 01:29:15 +0100

    added tests

--- src/test_md2txt.py
+++ src/test_md2txt.py
@@ -235,6 +235,18 @@ class Testmd2txt(unittest.TestCase):
                                      TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
                                      TextNode(" and a ", TextType.TEXT),
                                      TextNode("link", TextType.LINK, "https://boot.dev")])
+        
+    def test_text_to_textnodes_all_nodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and a [link](https://boot.dev)"
+        new_nodes = text_to_textnodes(text)
+        self.assertEqual(new_nodes, [TextNode("This is ", TextType.TEXT),
+                                     TextNode("text", TextType.BOLD),
+                                     TextNode(" with an ", TextType.TEXT),
+                                     TextNode("italic", TextType.ITALIC),
+                                     TextNode(" word and a ", TextType.TEXT),
+                                     TextNode("code block", TextType.CODE),
+                                     TextNode(" and a ", TextType.TEXT),
+                                     TextNode("link", TextType.LINK, "https://boot.dev")])
     
 
 if __name__ == "__main__":
