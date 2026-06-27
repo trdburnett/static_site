d92b6fbf91385d79140b95f14fd5f7f3c92b4014
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-27 22:03:36 +0100

    editing

--- src/test_md2txt.py
+++ src/test_md2txt.py
@@ -252,7 +252,7 @@ class Testmd2txt(unittest.TestCase):
         md = """# This is a heading
 
 This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
-        
+
 - This is the first list item in a list block
 - This is a list item
 - This is another list item"""
