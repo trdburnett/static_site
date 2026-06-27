df022f66f90a2fd42d42d3c37b62109cad3e3bd6
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-27 21:23:45 +0100

    editing

--- src/md2txt.py
+++ src/md2txt.py
@@ -133,6 +133,7 @@ markdown_to_blocks("""# This is a heading
 
 This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
 
+                   
 - This is the first list item in a list block
 - This is a list item
 - This is another list item""")
