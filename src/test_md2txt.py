2abe789384f3ae8e23635b2d211f58d08cb45802
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-27 01:23:02 +0100

    added test

--- src/test_md2txt.py
+++ src/test_md2txt.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from md2txt import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from md2txt import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
 
 class Testmd2txt(unittest.TestCase):
     def test_empty_list(self):
@@ -209,8 +209,20 @@ class Testmd2txt(unittest.TestCase):
         old_nodes = []
         with self.assertRaises(Exception):
             new_nodes = split_nodes_link(old_nodes)
-        
-        
+
+    def test_text_to_textnodes_all_nodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        new_nodes = text_to_textnodes(text)
+        self.assertEqual(new_nodes, [TextNode("This is ", TextType.TEXT),
+                                     TextNode("text", TextType.BOLD),
+                                     TextNode(" with an ", TextType.TEXT),
+                                     TextNode("italic", TextType.ITALIC),
+                                     TextNode(" word and a ", TextType.TEXT),
+                                     TextNode("code block", TextType.CODE),
+                                     TextNode(" and an ", TextType.TEXT),
+                                     TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                                     TextNode(" and a ", TextType.TEXT),
+                                     TextNode("link", TextType.LINK, "https://boot.dev")])
     
 
 if __name__ == "__main__":
