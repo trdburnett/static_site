796758af181fbdf0a0683c166c40a98bd0e19a2d
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-27 01:25:58 +0100

    added test

--- src/test_md2txt.py
+++ src/test_md2txt.py
@@ -224,6 +224,19 @@ class Testmd2txt(unittest.TestCase):
                                      TextNode(" and a ", TextType.TEXT),
                                      TextNode("link", TextType.LINK, "https://boot.dev")])
     
+    def test_text_to_textnode_missing_bold(self):
+        text = "This is  with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        new_nodes = text_to_textnodes(text)
+        self.assertEqual(new_nodes, [TextNode("This is ", TextType.TEXT),
+                                     TextNode(" with an ", TextType.TEXT),
+                                     TextNode("italic", TextType.ITALIC),
+                                     TextNode(" word and a ", TextType.TEXT),
+                                     TextNode("code block", TextType.CODE),
+                                     TextNode(" and an ", TextType.TEXT),
+                                     TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                                     TextNode(" and a ", TextType.TEXT),
+                                     TextNode("link", TextType.LINK, "https://boot.dev")])
+    
 
 if __name__ == "__main__":
     unittest.main()
