3a72073eb9f05ac0e28ce1806159822deb8ea679
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-27 21:35:24 +0100

    editing

--- src/md2txt.py
+++ src/md2txt.py
@@ -134,9 +134,9 @@ def markdown_to_blocks(markdown: str) -> list[str]:
 
 markdown_to_blocks("""# This is a heading
 
-This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
+    This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
 
                    
-- This is the first list item in a list block
+- This is the first list item in a list block    
 - This is a list item
 - This is another list item""")
