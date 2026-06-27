649c6ff7ca1d6f30fdd6b88d5e7cc3bb3336127c
trdburnett <terryronalddavidburnett@gmail.com>
2026-06-27 21:53:07 +0100

    implemented markdown_to_blocks_ready for testing and added a test

--- src/md2txt.py
+++ src/md2txt.py
@@ -130,13 +130,5 @@ def markdown_to_blocks(markdown: str) -> list[str]:
         stripped_whitespace = string.strip()
         stripped_newlines = stripped_whitespace.strip("\n")
         blocks.append(stripped_newlines)
-    print(blocks)
+    return blocks
 
-markdown_to_blocks("""# This is a heading
-
-    This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
-
-                   
-- This is the first list item in a list block
-- This is a list item
-- This is another list item   """)
--- src/test_md2txt.py
+++ src/test_md2txt.py
@@ -247,6 +247,20 @@ class Testmd2txt(unittest.TestCase):
                                      TextNode("code block", TextType.CODE),
                                      TextNode(" and a ", TextType.TEXT),
                                      TextNode("link", TextType.LINK, "https://boot.dev")])
+        
+    def test_markdown_to_blocks(self):
+        md = """# This is a heading
+
+This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
+
+                   
+- This is the first list item in a list block
+- This is a list item
+- This is another list item"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(blocks, ["# This is a heading",
+                                  "This is a paragraph of text. It has some **bold** and _italic_ words inside of it.",
+                                  "- This is the first list item in a list block\n- This is a list item\n- This is another list item"])
     
 
 if __name__ == "__main__":
