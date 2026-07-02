12af1644b967046c9a3a3cdfaddaa3da11dc02a2
trdburnett <terryronalddavidburnett@gmail.com>
2026-07-02 01:33:50 +0100

    modified a test to markdown_to_blocks

--- src/test_md2txt.py
+++ src/test_md2txt.py
@@ -269,7 +269,9 @@ This is a paragraph of text. It has some **bold** and _italic_ words inside of i
                    
 - This is the first list item in a list block
 - This is a list item
-- This is another list item"""
+- This is another list item
+
+"""
         blocks = markdown_to_blocks(md)
         self.assertEqual(blocks, ["# This is a heading",
                                   "This is a paragraph of text. It has some **bold** and _italic_ words inside of it.",
