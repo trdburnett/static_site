624d50c9c34f8de9d0be52016ef038f54daa75fe
trdburnett <terryronalddavidburnett@gmail.com>
2026-07-07 11:09:35 +0100

    editing

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-import textnode, os
+import textnode, os, shutil
 def main():
     dummynode = textnode.TextNode("some text", textnode.TextType.LINK, "https://www.boot.dev")
     print(dummynode)
 
 def generate_public():
+    print(os.path.exists("./public"))
+    shutil.rmtree("./public")
     print(os.path.exists("./public"))
 
 generate_public()
