e09a79219308347ced832341818582253cd2e408
trdburnett <terryronalddavidburnett@gmail.com>
2026-07-07 11:15:19 +0100

    directory

--- src/main.py
+++ src/main.py
@@ -4,8 +4,13 @@ def main():
     print(dummynode)
 
 def generate_public():
+    #checks if the given path exists
     print(os.path.exists("./public"))
+    #removes given directory
     shutil.rmtree("./public")
+    print(os.path.exists("./public"))
+    #makes given directory
+    os.mkdir("./public")
     print(os.path.exists("./public"))
 
 generate_public()
