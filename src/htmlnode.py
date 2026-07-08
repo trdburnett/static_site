53ec0025e809687b0d93f9d928a2dbadafe3c621
trdburnett <terryronalddavidburnett@gmail.com>
2026-07-08 16:49:05 +0100

    updated LeafNode.to_html() to correctly handle images and links

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -30,8 +30,13 @@ class LeafNode(HTMLNode):
             raise(ValueError)
         if self.tag == None:
             return str(self.value)
-        else:
+        if self.props == None:
             return f"<{self.tag}>{self.value}</{self.tag}>"
+        else:
+            if self.tag == "a":
+                return f"<{self.tag} href={self.props['href']}>{self.value}</{self.tag}"
+            if self.tag == "img":
+                return f"<{self.tag} src={self.props['src']} alt={self.props['alt']} />"
     
     def __repr__(self):
         return f"Leaf_Node({self.tag}, {self.value}, {self.props})"
