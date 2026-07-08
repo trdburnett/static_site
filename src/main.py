e33cd8503b353e0fae77ff1c198354b1cb3725ee
trdburnett <terryronalddavidburnett@gmail.com>
2026-07-08 14:28:46 +0100

    implemented generate_page and updated main.sh to start server using public dir

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import textnode, os, shutil
-from md2txt import extract_title    
+from md2txt import extract_title
+from md2html import markdown_to_html_node
+from htmlnode import HTMLNode    
 def main():
     generate_public()
-    dummynode = textnode.TextNode("some text", textnode.TextType.LINK, "https://www.boot.dev")
-    print(dummynode)
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 def generate_public(source="./static",destination="./public", clean=True):
     #checks to see if destination directory exists and if a clean directory is required removes the directory 
@@ -27,8 +28,20 @@ def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path}, to {dest_path} using {template_path}")
     from_file = open(from_path)
     markdown = from_file.read()
+    from_file.close()
     template_file = open(template_path)
     template = template_file.read()
-    
+    template_file.close()
+    content = markdown_to_html_node(markdown)
+    content_html = content.to_html()
+    title = extract_title(markdown)
+    template.replace("{{ Title }}", title)
+    template.replace("{{ Content }}", content_html)
+    dest_dir = os.path.dirname(dest_path)
+    os.makedirs(dest_dir, exist_ok=True)
+    dest_file = open(dest_path)
+    dest_file.write(template)
+    dest_file.close()
+
 
 main()
