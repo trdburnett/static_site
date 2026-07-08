1e37c43a08b6a00905c517c415a1e3e135a5b541
trdburnett <terryronalddavidburnett@gmail.com>
2026-07-08 15:48:47 +0100

    debugging

--- src/debug.py
+++ src/debug.py
@@ -55,13 +55,12 @@ This site was generated with a custom-built [static site generator](https://www.
 #    print(item)
 
 paragraph_blocks = []
-paragraph_text_nodes = ""
 for block in page_blocks:
     if block_to_block_type(block) == BlockType.PARAGRAPH:
         paragraph_blocks.append(block)
 for block in paragraph_blocks:
     #print(block)
-    paragraph_text_nodes.append(text_to_textnodes(block))
+    paragraph_text_nodes = text_to_textnodes(block)
 for node in paragraph_text_nodes:
     #print(node)
     print(text_node_to_html_node(node))
