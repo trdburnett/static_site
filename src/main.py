d334f2c4c9be4e658ddc968e543788f2c67c69ab
trdburnett <terryronalddavidburnett@gmail.com>
2026-07-12 23:55:06 +0100

    editing

--- src/main.py
+++ src/main.py
@@ -50,6 +50,7 @@ def generate_pages(dir_path_content, template_path, dest_dir_path):
     content_list = os.listdir(dir_path_content)
     print(content_list)
     for content in content_list:
+        print(os.path.isfile(content))
         if os.path.isfile(content):
             print("here")
             print(os.path.join(dir_path_content, content))
